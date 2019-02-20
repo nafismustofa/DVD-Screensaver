2a4ed60f5438199c580ec59d1f679e4c5e086b16
NFS101 <realnfs101@gmail.com>
2019-02-21 00:13:37 +0600

    Alpha 1.0

--- src/display.py
+++ src/display.py
@@ -14,11 +14,13 @@ class Display:
         #DVD Icon Variables
         self.__dvd_x = (self.__screen_width // 2) - 40
         self.__dvd_y = (self.__screen_height // 2) - 40
-        self.__dvd_speed = [random.choice([3 , -3]) , random.choice([3 , -3])]
+        self.__dvd_speed = [random.choice([4 , -4]) , random.choice([4 , -4])]
+        self.__side_hit = 0
+        self.__corner_hit = 0
 
         #Objects
         self.__game_display = None
-        self.__icon = pygame.image.load("./assets/DVD_icon.png")
+        self.__icon = pygame.image.load("./assets/DVD_logo.png")
         self.__clock = pygame.time.Clock()
 
     #Display DVD
@@ -29,17 +31,42 @@ class Display:
         self.__dvd_x += self.__dvd_speed[0]
         self.__dvd_y += self.__dvd_speed[1]
 
-        #Collision Check
+        #Side Collision Check
         if self.__dvd_x <= 0 or (self.__dvd_x >= self.__screen_width - 75):
+            self.__side_hit += 1
             self.__dvd_speed[0] = -self.__dvd_speed[0]
-        if self.__dvd_y <= 15 or self.__dvd_y >=  (self.__screen_height - 15):
+        if self.__dvd_y <= -20 or self.__dvd_y >=  (self.__screen_height - 60):
+            self.__side_hit += 1
             self.__dvd_speed[1] = -self.__dvd_speed[1]
 
-        self.__game_display.blit(dvd , [self.__dvd_x , self.__dvd_y - 40])
+        #Corner Collision Check
+        if self.__dvd_x == 0 and self.__dvd_y == 0:
+            self.__corner_hit += 1
+        if self.__dvd_x == 0 and self.__dvd_y == (self.__screen_height - 60):
+            self.__corner_hit += 1
+        if self.__dvd_x == (self.__screen_width - 80) and self.__dvd_y == (self.__screen_height - 60):
+            self.__corner_hit += 1
+        if self.__dvd_x == (self.__screen_width - 80) and self.__dvd_y == 0:
+            self.__corner_hit += 1
+
+        self.__game_display.blit(dvd , [self.__dvd_x , self.__dvd_y])
 
     #Display Stats
     def __stats(self):
-        pass
+        text_font = pygame.font.SysFont(None , 25)
+
+        corner_hit_text = text_font.render("Corner Hit: {}" .format(self.__corner_hit) , True , self.__color_white)
+        corner_rect = corner_hit_text.get_rect()
+        side_hit_text = text_font.render("Side Hit: {}" .format(self.__side_hit) , True , self.__color_white)
+        side_rect = side_hit_text.get_rect()
+
+        corner_rect.centerx = 100
+        corner_rect.centery = 500
+        side_rect.centerx = 100
+        side_rect.centery = 525
+
+        self.__game_display.blit(corner_hit_text , corner_rect)
+        self.__game_display.blit(side_hit_text , side_rect)
 
     #Main Display Function
     def display(self):
@@ -64,6 +91,9 @@ class Display:
 
             #Display DVD Icon
             self.__dvd()
+
+            #Display Text
+            self.__stats()
 
             #Display Update
             pygame.display.update()
